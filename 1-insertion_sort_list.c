4f7a67a2ecaebe9b0b870c8bece5e09a139d0dc6
Wise2525 <ayesiganobert@gmail.com>
2021-10-25 02:44:11 -0700

    big O🕊🕊🕊

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,64 +1,51 @@
 #include "sort.h"
 
-
 /**
- * swapprev - swaps a node with the previous node in a doubly linked list
- * @current: the current node
- * @head: head of the doubly linked list
+ * insertion_sort_list - sorts a linked list ussing insertion sorting algorithm
+ * @list: pointer to the head of the list
+ * Return: void
  */
-void swapprev(listint_t *current, listint_t **head)
-{
-	listint_t *tmp = NULL;
 
-	tmp = current->prev;
-	if (current->next)
-		current->next->prev = tmp;
-	if (tmp->prev)
-		tmp->prev->next = current;
-	else
-		*head = current;
-
-	current->prev = current->prev->prev;
-	tmp->next = current->next;
-	tmp->prev = current;
-	current->next = tmp;
-}
-/**
- * insertion_sort_list - implemetnation of insertion sort in
- * a doubly linked list
- * @list: a pointer to the head of the list to be sorted
- */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL, *prev_tmp = NULL, *prev = NULL;
+	listint_t *head = *list, *current, *tmp;
 
-	if (!list)
+	if (list == NULL || (*list) == NULL || head == NULL || head->next == NULL)
 		return;
-	current = *list;
-	while (current)
-	{
-		prev = current->prev;
-		/*move the current element some steps back until in position*/
-		while (prev)
-		{
-			if (current->n >= prev->n)
-				break;
-			/*swap the two nodes if they are out of order*/
-			swapprev(current, list);
 
-			/*once the swap is done swap the names*/
-			prev_tmp = prev;
-			prev = current;
-			current = prev_tmp;
-			print_list(*list);
-			/*
-			 * go back one step so that the newly swaped(went to left) value
-			 * will be your current and make sure it isn't smaller than
-			 * the value in its left
-			 */
-			prev = prev->prev;
-			current = current->prev;
+	head = head->next;
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		tmp = current->prev;
+		while (current && tmp)
+		{
+			if (current->n < tmp->n)
+			{
+				if (current->prev != NULL)
+				{
+					current->prev->next = current->next;
+				}
+				if (current->next != NULL)
+				{
+					current->next->prev = current->prev;
+				}
+				current->next = tmp;
+				if (tmp->prev)
+					tmp->prev->next = current;
+				current->prev = tmp->prev;
+				tmp->prev = current;
+				if (current->prev == NULL)
+				{
+					*list = current;
+					print_list(*list);
+					break;
+				}
+				tmp = current->prev;
+				print_list(*list);
+			} else
+				tmp = tmp->prev;
 		}
-		current = current->next;
 	}
 }
