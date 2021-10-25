c010d97fb9e2862e98040bd651f8cdc85db70aca
Wise2525 <ayesiganobert@gmail.com>
2021-10-25 02:41:13 -0700

    big O🕊🕊🕊

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,64 @@
 #include "sort.h"
 
+
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
- * order using the Insertion sort algorithm
- * @list: Double pointer to the head of the linked list
- *
- * Return: void
+ * swapprev - swaps a node with the previous node in a doubly linked list
+ * @current: the current node
+ * @head: head of the doubly linked list
+ */
+void swapprev(listint_t *current, listint_t **head)
+{
+	listint_t *tmp = NULL;
+
+	tmp = current->prev;
+	if (current->next)
+		current->next->prev = tmp;
+	if (tmp->prev)
+		tmp->prev->next = current;
+	else
+		*head = current;
+
+	current->prev = current->prev->prev;
+	tmp->next = current->next;
+	tmp->prev = current;
+	current->next = tmp;
+}
+/**
+ * insertion_sort_list - implemetnation of insertion sort in
+ * a doubly linked list
+ * @list: a pointer to the head of the list to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *swap_node, *next_swap;
+	listint_t *current = NULL, *prev_tmp = NULL, *prev = NULL;
 
-	if (list == NULL || *list == NULL)
+	if (!list)
 		return;
-	swap_node = (*list)->next;
-	while (swap_node != NULL)
+	current = *list;
+	while (current)
 	{
-		next_swap = swap_node->next;
-		while (swap_node->prev != NULL && swap_node->prev->n > swap_node->n)
+		prev = current->prev;
+		/*move the current element some steps back until in position*/
+		while (prev)
 		{
-			swap_node->prev->next = swap_node->next;
-			if (swap_node->next != NULL)
-				swap_node->next->prev = swap_node->prev;
-			swap_node->next = swap_node->prev;
-			swap_node->prev = swap_node->next->prev;
-			swap_node->next->prev = swap_node;
-			if (swap_node->prev == NULL)
-				*list = swap_node;
-			else
-				swap_node->prev->next = swap_node;
+			if (current->n >= prev->n)
+				break;
+			/*swap the two nodes if they are out of order*/
+			swapprev(current, list);
+
+			/*once the swap is done swap the names*/
+			prev_tmp = prev;
+			prev = current;
+			current = prev_tmp;
 			print_list(*list);
+			/*
+			 * go back one step so that the newly swaped(went to left) value
+			 * will be your current and make sure it isn't smaller than
+			 * the value in its left
+			 */
+			prev = prev->prev;
+			current = current->prev;
 		}
-		swap_node = next_swap;
+		current = current->next;
 	}
 }
