3c280c48c2d03166437c29be51fe7e451a86cec2
Wise2525 <ayesiganobert@gmail.com>
2021-10-25 03:15:28 -0700

    sort🕊🕊🕊

--- sort.h
+++ sort.h
@@ -32,24 +32,4 @@ void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
 
-void cocktail_sort_list(listint_t **list);
-
-int split(int *arr, int left, int right, size_t size);
-
-void sort_alg(int *arr, int left, int right, size_t size);
-
-void shell_sort(int *array, size_t size);
-
-void quick_sort_hoare(int *array, size_t size);
-
-void bitonic_sort(int *array, size_t size);
-
-void radix_sort(int *array, size_t size);
-
-void heap_sort(int *array, size_t size);
-
-void merge_sort(int *array, size_t size);
-
-void counting_sort(int *array, size_t size);
-
 #endif
