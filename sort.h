ca0334da01f41b2e7da6c126f01cea7d069b2ba0
firdaushassansalim <firdaushassan033@gmail.com>
2021-10-26 11:49:03 +0300

    added more funcs()

--- sort.h
+++ sort.h
@@ -1,9 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
-/*Includes*/
-#include <stdio.h>
 
-/*Structs*/
+/*includes*/
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,18 +19,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*Prototypes*/
-
+/*Given printing functions*/
 void print_list(const listint_t *list);
-
 void print_array(const int *array, size_t size);
 
+/*Sorting algorithms*/
 void bubble_sort(int *array, size_t size);
-
 void insertion_sort_list(listint_t **list);
-
 void selection_sort(int *array, size_t size);
-
 void quick_sort(int *array, size_t size);
 
 #endif
