dd8ce1401780efd91369569670425e56df6ed364
firdaushassansalim <firdaushassan033@gmail.com>
2021-10-26 15:41:23 +0300

    added bitonic parameters

--- sort.h
+++ sort.h
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
 /**
  * enum bool - Enumeration of Boolean values.
  * @false: Equals 0.
