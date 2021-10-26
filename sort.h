4d6d99aaac99bfde497ec12f4b2fb18b97873fbe
firdaushassansalim <firdaushassan033@gmail.com>
2021-10-26 12:57:16 +0300

    added bool struct

--- sort.h
+++ sort.h
@@ -6,6 +6,17 @@
 #include <stdlib.h>
 
 /**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+/**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
@@ -29,5 +40,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
